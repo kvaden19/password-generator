090fe452170bbafad2a6aaba86aea713e43e86c3
Kelly Vaden <klvaden@gmail.com>
2021-02-23 08:10:25 -0500

    Added input validations

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # password-generator
 
 # TODO:
-- Code validations
 - Code password generator
 - Testing testing testing
+
+## Future Work:
+- Instead of 4 prompts for character set, show the user a list and allow them to select what they want
+- "Cancel" on any of the character prompts breaks out of the validation loop
+- Customize "This page says..."
--- script.js
+++ script.js
@@ -4,23 +4,47 @@ var generateBtn = document.querySelector("#generate"); // ties JS variable to th
 // Write password to the #password input
 function writePassword() {
   window.alert("Let's make you a password!");
-    // Prompt for character types to include in the password (lowercase, uppercase, numeric, and/or special characters)
-      // Validate for at least one
-      var upper = window.prompt('Does your password need uppercase characters? [Y/N] \n Examples: A, B, C', 'Y');
-      var lower = window.prompt('Does your password need lowercase characters? [Y/N] \n Examples: a, b, c', 'Y');
-      var num = window.prompt('Does your password need numeric characters? [Y/N] \n Examples: 1, 2, 3', 'Y');
-      var special = window.prompt('Does your password need special characters? [Y/N] \n Examples: !, %, $', 'Y');
+  // Set up variables to represent choice of character sets and password length
+  var upper = 'N';
+  var lower = 'N';
+  var num = 'N';
+  var special = 'N';
+  var length = 0;
 
-    // Prompt for length (at least 8, no more than 128)
-      // Validate
-      var length = window.prompt('How long do you need the password to be? \n Must be at least 8 characters and no longer than 128.', '12');
+  // Prompt for character types to include in the password (lowercase, uppercase, numeric, and/or special characters)
+  // Convert user input to upper case
+  // Validate that at least one chracter set is chosen before moving on
 
-    // Generate the password:
-      // Create arrays for each character type
-      var specialList =  ["!", "$", "%", "&", '"', "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", '\\', ']', '^', "`", "{", "|", "}", "~", '_'];
+  while (upper != 'Y' && lower != 'Y' && num != 'Y' && special != 'Y') {
+    var upper = window.prompt('Does your password need uppercase characters? [Y/N]\nExamples: A, B, C', 'Y');
+    upper = upper.toUpperCase();
+    var lower = window.prompt('Does your password need lowercase characters? [Y/N]\nExamples: a, b, c', 'Y');
+    lower = lower.toUpperCase();
+    var num = window.prompt('Does your password need numeric characters? [Y/N]\nExamples: 1, 2, 3', 'Y');
+    num = num.toUpperCase();
+    var special = window.prompt('Does your password need special characters? [Y/N]\nExamples: !, %, $', 'Y');
+    special = special.toUpperCase();
+    if (upper != 'Y' && lower != 'Y' && num != 'Y' && special != 'Y') {
+      window.alert('I need something to work with...\nPlease choose at least one character type.')
+    }
+  }
 
-      // Concatenate arrays user wants
-      // Pick # of elements from array depending on legnth
+  // Prompt for length and validate that length is at least 8, no more than 128 characters
+  while (length < 8 || length > 128) {
+    var length = window.prompt('How long do you need the password to be?\nMust be at least 8 characters and no longer than 128.', '12');
+  }
+
+  // Generate the password:
+  // Create arrays for each character type
+  var upperList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var lowerList = upperList.toLowerCase();
+  var numList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+  var specialList =  ["!", "$", "%", "&", '"', "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", '\\', ']', '^', "`", "{", "|", "}", "~", '_'];
+
+  // Make a combined array of the character sets user wants
+  var combinedList = [];
+
+  // Pick [length] # of elements from the combined array
 
   // var password = function generatePassword() { // function to generate password
   //   return "example"; 
