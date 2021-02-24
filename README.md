414a86bff8e37fdaa820b122e39b098bd8529d1e
Kelly Vaden <klvaden@gmail.com>
2021-02-23 20:05:58 -0500

    Minimum viable product - password generator is working

--- README.md
+++ README.md
@@ -1,10 +1,52 @@
 # password-generator
 
-# TODO:
-- Code password generator
-- Testing testing testing
+## TODO:
+- Use the given function structure
+- Technically your algorithm might not pick from each of the selected criteria
+    - Divide desired length into 1-4 buckets
+    - Choose characters from each
+- Testing
+- Add the break loop for cancel
+    if (!userChoice) {
+	return;
+}
 
-## Future Work:
+
+## Future Improvements:
 - Instead of 4 prompts for character set, show the user a list and allow them to select what they want
 - "Cancel" on any of the character prompts breaks out of the validation loop
 - Customize "This page says..."
+
+### Tests
+
+#### Validation
+##### Acceptance Criteria: User sees expected error message
+- No character sets
+- Length < 8
+- Length > 128
+
+#### Password Generation
+##### Acceptance Criteria: Password is required length and includes required characters
+- All character sets
+- 3 character sets
+    - Upper, lower, special
+    - Upper, lower, numerical
+    - Upper, special, numerical
+    - Lower, numerical, special
+- 2 character sets
+    - Upper and lower
+    - Upper and numerical
+    - Upper and special
+    - Lower and numerical
+    - Lower and special
+    - Numerical and special
+- 1 character set
+    - Upper
+    - Lower
+    - Numerical
+    - Special
+- Length = 8
+- Length = 128
+- Length = 16
+- Length = 40
+- Length = 75
--- script.js
+++ script.js
@@ -5,58 +5,71 @@ var generateBtn = document.querySelector("#generate"); // ties JS variable to th
 function writePassword() {
   window.alert("Let's make you a password!");
   // Set up variables to represent choice of character sets and password length
-  var upper = 'N';
-  var lower = 'N';
-  var num = 'N';
-  var special = 'N';
+  var upper = "N";
+  var lower = "N";
+  var num = "N";
+  var special = "N";
   var length = 0;
 
   // Prompt for character types to include in the password (lowercase, uppercase, numeric, and/or special characters)
   // Convert user input to upper case
   // Validate that at least one chracter set is chosen before moving on
-
-  while (upper != 'Y' && lower != 'Y' && num != 'Y' && special != 'Y') {
-    var upper = window.prompt('Does your password need uppercase characters? [Y/N]\nExamples: A, B, C', 'Y');
+  while (upper != "Y" && lower != "Y" && num != "Y" && special != "Y") {
+    var upper = window.prompt("Does your password need uppercase characters? [Y/N]\nExamples: A, B, C", "Y");
     upper = upper.toUpperCase();
-    var lower = window.prompt('Does your password need lowercase characters? [Y/N]\nExamples: a, b, c', 'Y');
+    var lower = window.prompt("Does your password need lowercase characters? [Y/N]\nExamples: a, b, c", "Y");
     lower = lower.toUpperCase();
-    var num = window.prompt('Does your password need numeric characters? [Y/N]\nExamples: 1, 2, 3', 'Y');
+    var num = window.prompt("Does your password need numeric characters? [Y/N]\nExamples: 1, 2, 3", "Y");
     num = num.toUpperCase();
-    var special = window.prompt('Does your password need special characters? [Y/N]\nExamples: !, %, $', 'Y');
+    var special = window.prompt("Does your password need special characters? [Y/N]\nExamples: !, %, $", "Y");
     special = special.toUpperCase();
-    if (upper != 'Y' && lower != 'Y' && num != 'Y' && special != 'Y') {
-      window.alert('I need something to work with...\nPlease choose at least one character type.')
+    if (upper != "Y" && lower != "Y" && num != "Y" && special != "Y") {
+      window.alert("I need something to work with...\nPlease choose at least one character type.")
     }
   }
 
   // Prompt for length and validate that length is at least 8, no more than 128 characters
   while (length < 8 || length > 128) {
-    var length = window.prompt('How long do you need the password to be?\nMust be at least 8 characters and no longer than 128.', '12');
+    var length = window.prompt("How long do you need the password to be?\nMust be at least 8 characters and no longer than 128.", "12");
   }
 
   // Generate the password:
   // Create arrays for each character type
-  var upperList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  var lowerList = upperList.toLowerCase();
-  var numList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-  var specialList =  ["!", "$", "%", "&", '"', "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", '\\', ']', '^', "`", "{", "|", "}", "~", '_'];
+  var upperList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
+  var lowerList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "x"];
+  var numList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var specialList =  ["!", "$", "%", "&", "\"", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "`", "{", "|", "}", "~", "_"];
 
   // Make a combined array of the character sets user wants
   var combinedList = [];
+  if (upper === "Y") {
+    combinedList = combinedList.concat(upperList);
+  }
+  if (lower === "Y") {
+    combinedList = combinedList.concat(lowerList);
+  }
+  if (num === "Y") {
+    combinedList = combinedList.concat(numList);
+  }
+  if (special === "Y") {
+    combinedList = combinedList.concat(specialList);
+  }
 
   // Pick [length] # of elements from the combined array
+  var password = "";
+
+  for (var i=0; i < length; i++) {
+    var index = Math.floor(Math.random() * combinedList.length);
+    password = password + combinedList[index];
+  }
 
   // var password = function generatePassword() { // function to generate password
   //   return "example"; 
   // };
 
   var passwordText = document.querySelector("#password"); // ties JS variable to the HTML text box
-  var password = "example";
   passwordText.value = password; // puts output of generatePassword in the text box
 };
 
 // When button is clicked, call the writePassword function
 generateBtn.addEventListener("click", writePassword);
-
-
-
